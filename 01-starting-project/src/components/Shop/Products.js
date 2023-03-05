061e6b48170ca12ca40f95686c623955c24d2288
karthik-140 <shanigaramkarthik@gmail.com>
2023-03-05 18:07:28 +0530

    making-cart-specific

--- 01-starting-project/src/components/Shop/Products.js
+++ 01-starting-project/src/components/Shop/Products.js
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 4,
+    title: "Book",
+    description: "My First Book"
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Bottle",
+    description: "For Drinking"
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+
       </ul>
     </section>
   );
